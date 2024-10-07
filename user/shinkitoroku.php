de24a366a8e2fa0ec5397ef7abce8b214148a89d
fsho1222 <148302976+fsho1222@users.noreply.github.com>
2024-10-07 12:34:01 +0900

    新規登録 仮

---
+++ user/shinkitoroku.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>新規登録</title>
+</head>
+<body>
+    <form>
+        <label for="email">メールアドレス:</label>
+        <input type="email" id="email" name="email"><br><br>
+
+        <label for="password">パスワード:</label>
+        <input type="password" id="password" name="password"><br><br>
+
+        <label for="username">ユーザーネーム:</label>
+        <input type="text" id="username" name="username"><br><br>
+
+        <button type="submit">新規登録</button>
+    </form>
+</body>
+</html>
