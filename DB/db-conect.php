c7043a5916e9844179f65995839621a1f6ed1d35
Nozakiishin <148302881+Nozakiishin@users.noreply.github.com>
2024-10-07 12:45:30 +0900

    db-conect

---
+++ DB/db-conect.php
@@ -0,0 +1,8 @@
+<?php
+    const SERVER = 'mysql310.phy.lolipop.lan';
+    const DBNAME = 'LAA1517446-lowcode';
+    const USER = 'LAA1517446';
+    const PASS = 'lowcode';
+ 
+    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+?>
