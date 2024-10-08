81947d31314826db91fd54a689cbf31665f98c68
fsho1222 <148302976+fsho1222@users.noreply.github.com>
2024-10-08 13:50:14 +0900

    henko

--- user/shinkitoroku.php
+++ user/shinkitoroku.php
@@ -1,3 +1,12 @@
+<?php
+    const SERVER = 'mysql302.phy.lolipop.lan';
+    const DBNAME = 'LAA1517442-postingapp24';
+    const USER = 'LAA1517442';
+    const PASS = 'post0418';
+ 
+    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
