c78e2ec6a42fee431429776e1c18d8feb96bb048
Nozakiishin <148302881+Nozakiishin@users.noreply.github.com>
2024-10-11 10:20:05 +0900

    a

--- user/home.php
+++ user/home.php
@@ -3,9 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../css/home.css" rel="stylesheet">
     <title>ホーム画面</title>
+   
 </head>
 <body>
-    
+    <div>
+       <h3> <button class="home-button" onclick="location.href='editor.html'">新規プロジェクト作成</button></h3>
+        
+        <!-- プロジェクト管理画面への遷移ボタン -->
+      <h3>  <button class="home-button" onclick="location.href='project-kanri.html'">プロジェクト管理</button></h3>
+        
+        <!-- アカウント管理画面への遷移ボタン -->
+       <h3> <button class="home-button" onclick="location.href='account-kanri.html'">アカウント管理</button></h3>
+    </div>
 </body>
 </html>
