3f9a6bb8349b529d6d3da4335cb64137797b59f7
fsho1222 <148302976+fsho1222@users.noreply.github.com>
2024-11-18 11:52:22 +0900

    h

--- DB/db-connect.php
+++ DB/db-connect.php
@@ -5,4 +5,17 @@
     const PASS = 'lowcode';
  
     $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+?>try {
+    $pdo = new PDO(
+        'mysql:host=' . SERVER . ';dbname=' . DBNAME . ';charset=utf8',
+        USER,
+        PASS,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        ]
+    );
+} catch (PDOException $e) {
+    die("データベース接続エラー: " . $e->getMessage());
+}
 ?>
